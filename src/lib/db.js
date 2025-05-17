ee52c074f46982c3989ae1e489c377f9cdc94b4c
harsh-coder-desgin <harshpatel51057@gmail.com>
2025-05-17 16:30:09 +0530

    Update db.js

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,1 +1,1 @@
-export const connectionSrt ="mongodb+srv://patel:google@cluster0.9r4xr.mongodb.net/RestaurantsDB?retryWrites=true&w=majority&appName=Cluster0"
+export const connectionSrt ="mongodb+srv://"+username+":"+password+"e@cluster0.9r4xr.mongodb.net/RestaurantsDB?retryWrites=true&w=majority&appName=Cluster0"
